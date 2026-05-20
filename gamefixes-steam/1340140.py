9591bd0e536eecdf163e8874e2647d59b6663e83
superboo07 <39241538+superboo07@users.noreply.github.com>
2026-05-20 16:14:25 -0400

    thus far

--- gamefixes-steam/1340140.py
+++ gamefixes-steam/1340140.py
@@ -5,8 +5,8 @@ from protonfixes import util
 
 def main() -> None:
     util.disable_protonmediaconverter()
+
     util.protontricks('mf')
-    util.protontricks('quartz')
-    util.protontricks('amstream')
+    util.protontricks('directshow')
     util.protontricks('wmp11')
 
